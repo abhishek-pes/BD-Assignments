09b01fc09b9b8fe5a127cd4a860dd45de0a9a3b4
abhishek mishra <abhishekmis40@gmail.com>
2021-09-20 11:59:11 +0530

    code update

--- A1_Road_accident_analysis/Task-2/mapper.py
+++ A1_Road_accident_analysis/Task-2/mapper.py
@@ -1,55 +1,51 @@
 #!/usr/bin/env python3
 
 import sys
-from math import radians, cos, sin, asin, sqrt
+import math
 import requests
 import json
-latitude = sys.argv[1]
-longitude = sys.argv[2]
-d = sys.argv[3]
-
+try:
+	latitude = sys.argv[1]
+	longitude = sys.argv[2]
+	d = sys.argv[3]
+except:
+	arguments = sys.argv[1].split(" ")
+	latitude = arguments[0]
+	longitude = arguments[1]
+	d = arguments[2]
 def make_req(lat,lon):
 	try:
 		pload = {"latitude": lat,"longitude":lon}
 		headers = {'Content-Type': 'application/json'}
 		r = requests.post('http://20.185.44.219:5000/',json=pload)
-		#print(r.text)
-		res = json.loads(r.text)
-		print(res["state"].strip(),'|',res["city"].strip().replace(" ","%20"),'|',1);
-		#print("%s\t%s\t%s"%(res["state"].strip(),res["city"].strip(),1))
+		return r.text
+		#res = json.loads(r.text)
+		#print(res["state"].strip(),'|',res["city"].strip().replace(" ","%20"),'|',1);
+		#print("%s\t%s"%(res["state"].strip(),res["city"].strip()))
 	except:
 		return
 
-def haversine(lon1, lat1, lon2, lat2):
-    """
-    Calculate the great circle distance between two points 
-    on the earth (specified in decimal degrees)
-    """
-
-    # convert decimal degrees to radians 
-    #lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
-    # haversine formula 
-    dlon = lon2 - lon1 
-    dlat = lat2 - lat1 
-    #print(dlon,dlat)
-    ed = (sqrt((lon2 - lon1)**2 + (lat1 - lat2)**2))
-    return ed
-    #a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
-    #c = 2 * asin(sqrt(a)) 
-    #print(c)
-    # Radius of earth in kilometers is 6371
-    # km = 6371* c
-    #return km
+def euclid(lon1, lat1, lon2, lat2):
+	try:
+		ed = (math.sqrt((lon2 - lon1)**2 + (lat1 - lat2)**2))
+		return ed
+	except:
+		return -1
     
 for line in sys.stdin:
 	line = line.strip()
 	loaded = json.loads(line)
 	try:
-		euclid_dist = haversine(float(loaded['Start_Lng']),float(loaded['Start_Lat']),float(longitude),float(latitude))
+		#print(type(str(loaded['End_Lat'])))
+		if(str(loaded['Start_Lat']) == "nan" or str(loaded['Start_Lng']) == "nan"):
+			continue
+		euclid_dist = euclid(float(loaded['Start_Lng']),float(loaded['Start_Lat']),float(longitude),float(latitude))
 		if(euclid_dist <= float(d)):
-			make_req(float(loaded['Start_Lat']),float(loaded['Start_Lng']))
-			#print('The distance is ',euclid_dist)
+			res = json.loads(make_req(float(loaded['Start_Lat']),float(loaded['Start_Lng'])))
+			if(res['request-status'] == 'success' and res):
+				if(res['state'] and ['city']):
+					print("%s\t%s"%(res['state'],res['city']))
 		else:
 			continue
-	except:
+	except Exception as e:
 		continue
--- A1_Road_accident_analysis/Task-2/reducer.py
+++ A1_Road_accident_analysis/Task-2/reducer.py
@@ -7,41 +7,35 @@ current_count = 0
 total_count = 0
 
 for line in sys.stdin:
-
-	line = line.strip()
-	#print(line.split('|'),2)
-	#print(line.split('|'))
-	#state = line.split('|').strip()
-	#print(state)
-	#state,city,count = line.split('|',2)
-	#print(state,city,count)
-	state,city,count = line.split('|',2)
-	count = int(count)
-	flag = 1
 	try:
-		city = city.replace("%20"," ")
+		line = line.strip()
+		state,city = line.split('\t',1)
+		count = 1
+		flag = 1
+		try:
+			city = city.replace("%20"," ")
+		except:
+			continue
+		if(current_state != state):
+			if(total_count != 0):
+				flag = 0
+				print(current_city.strip(),current_count)
+				print(current_state.strip(),total_count)
+				total_count = 0
+			print(state.strip())
+			
+		if current_city == city:
+			current_count += count
+		else:
+			if current_city and flag!=0:
+				print(current_city.strip(),current_count)
+			current_city = city
+			current_count = count
+			current_state = state
+		total_count += 1
 	except:
 		continue
-	if(current_state != state):
-		if(total_count != 0):
-			flag = 0
-			print(current_city.strip(),current_count)
-			print(current_state.strip(),total_count)
-			total_count = 0
-		print(state.strip())
-			
-	if current_city == city:
-		current_count += count
-	else:
-		if current_city and flag!=0:
-			print(current_city.strip(),current_count)
-			#print("%s\t%s"%(current_city,current_count))
-		current_city = city
-		current_count = count
-		current_state = state
-	total_count += 1
 if current_city == city:
 	print(current_city.strip(),current_count)
 	if(total_count != 0):
 		print(current_state.strip(),total_count)
-	#print("%s\t%s"%(current_city,current_count))
