8b8680cafdf6e85826cc08314b973f27f621cba0
abhishek mishra <abhishekmis40@gmail.com>
2021-09-22 10:00:06 +0530

    updated task2 code

--- A1_Road_accident_analysis/Task-2/mapper.py
+++ A1_Road_accident_analysis/Task-2/mapper.py
@@ -5,43 +5,35 @@ import math
 import requests
 import json
 try:
-	latitude = sys.argv[1]
-	longitude = sys.argv[2]
-	d = sys.argv[3]
+	latitude = float(sys.argv[1])
+	longitude = float(sys.argv[2])
+	d = float(sys.argv[3])
 except:
 	arguments = sys.argv[1].split(" ")
-	latitude = arguments[0]
-	longitude = arguments[1]
-	d = arguments[2]
-def make_req(lat,lon):
-	try:
-		pload = {"latitude": lat,"longitude":lon}
-		headers = {'Content-Type': 'application/json'}
-		r = requests.post('http://20.185.44.219:5000/',json=pload)
-		res = json.loads(r.text)
-		print(res["state"].strip(),'|',res["city"].strip().replace(" ","%20"));
-		#print("%s\t%s"%(res["state"].strip(),res["city"].strip()))
-	except:
-		return
+	latitude = float(arguments[0])
+	longitude = float(arguments[1])
+	d = float(arguments[2])
 
-def euclid(lon1, lat1, lon2, lat2):
-	try:
-		ed = (math.sqrt((lon2 - lon1)**2 + (lat1 - lat2)**2))
-		return ed
-	except:
-		return -1
-    
+def make_req(lat,lon):	
+	pload = {"latitude": lat,"longitude":lon }
+	r = requests.post('http://20.185.44.219:5000/',json=pload)
+	return r.json()
+
+count = 0
+latarr = []
+lonarr = []    
 for line in sys.stdin:
 	line = line.strip()
 	loaded = json.loads(line)
 	try:
-		#print(type(str(loaded['End_Lat'])))
-		if(str(loaded['Start_Lat']) == "nan" or str(loaded['Start_Lng']) == "nan"):
-			continue
-		euclid_dist = euclid(float(loaded['Start_Lng']),float(loaded['Start_Lat']),float(longitude),float(latitude))
-		if(euclid_dist <= float(d)):
-			make_req(float(loaded['Start_Lat']),float(loaded['Start_Lng']))
-		else:
-			continue
+		lat = float(loaded['Start_Lat'])
+		long = float(loaded['Start_Lng'])
+		if (math.sqrt((long - longitude)**2 + (lat - latitude)**2)) <= d:
+			res = make_req(lat,long)
+			try:
+				city = res["city"].split(" ")
+				print(res["state"],city[0]+"%20"+city[1].replace("-","|")+"z")
+			except:
+				print(res["state"],res["city"].replace("-","|")+"z")
 	except Exception as e:
 		continue
--- A1_Road_accident_analysis/Task-2/reducer.py
+++ A1_Road_accident_analysis/Task-2/reducer.py
@@ -3,19 +3,25 @@
 import sys
 current_state = None
 current_city = None
+current_der = None
 current_count = 0
 total_count = 0
 
 for line in sys.stdin:
 	try:
 		line = line.strip()
-		state,city = line.split('|',1)
+		state,city = line.split(' ',1)
 		count = 1
 		flag = 1
+
 		try:
 			city = city.replace("%20"," ")
+			city = city.replace("|","-")
+			city = city.rstrip(city[-1])
+
 		except:
 			continue
+
 		if(current_state != state):
 			if(total_count != 0):
 				flag = 0
